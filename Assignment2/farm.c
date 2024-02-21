7d34473f9b8a9cc3b6f32818024c37f8fce517a8
Koray Özgür <120639327+koozgur@users.noreply.github.com>
2024-02-21 14:19:12 +0300

    source code of gadgets

---
+++ Assignment2/farm.c
@@ -0,0 +1,131 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+unsigned getval_465()
+{
+    return 3351742792U;
+}
+
+void setval_297(unsigned *p)
+{
+    *p = 3364440122U;
+}
+
+unsigned getval_291()
+{
+    return 2425393233U;
+}
+
+unsigned getval_274()
+{
+    return 3281017213U;
+}
+
+void setval_176(unsigned *p)
+{
+    *p = 3287976392U;
+}
+
+unsigned getval_342()
+{
+    return 3285057864U;
+}
+
+unsigned addval_101(unsigned x)
+{
+    return x + 2445838664U;
+}
+
+void setval_333(unsigned *p)
+{
+    *p = 2496170312U;
+}
+
+unsigned getval_434()
+{
+    return 3347663591U;
+}
+
+void setval_384(unsigned *p)
+{
+    *p = 3254421832U;
+}
+
+void setval_394(unsigned *p)
+{
+    *p = 2438535389U;
+}
+
+unsigned addval_127(unsigned x)
+{
+    return x + 3284633928U;
+}
+
+void setval_335(unsigned *p)
+{
+    *p = 2429995336U;
+}
+
+unsigned addval_135(unsigned x)
+{
+    return x + 3285057608U;
+}
+
+void setval_296(unsigned *p)
+{
+    *p = 2446197064U;
+}
+
+unsigned getval_425()
+{
+    return 3456190534U;
+}
+
+unsigned addval_405(unsigned x)
+{
+    return x + 2432338248U;
+}
+
+void setval_340(unsigned *p)
+{
+    *p = 3284633928U;
+}
+
+unsigned addval_174(unsigned x)
+{
+    return x + 3287976264U;
+}
+
+unsigned addval_234(unsigned x)
+{
+    return x + 3284699464U;
+}
+
+unsigned addval_236(unsigned x)
+{
+    return x + 2497087816U;
+}
+
+unsigned getval_449()
+{
+    return 3285616968U;
+}
+
+unsigned getval_218()
+{
+    return 1504282526U;
+}
+
+void setval_440(unsigned *p)
+{
+    *p = 2429978952U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
